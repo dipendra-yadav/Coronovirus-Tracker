910df098ce9d4d54aba6436c58fe3ba41047a0c4
dipendra-yadav <dipendra.eng@gmail.com>
2020-03-08 12:49:51 +0530

    commit.

---
+++ coronavirus-tracker/src/test/java/io/deependra/coronavirustracker/CoronavirusTrackerApplicationTests.java
@@ -0,0 +1,12 @@
+package io.deependra.coronavirustracker;
+
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+
+/*
+ * @SpringBootTest class CoronavirusTrackerApplicationTests {
+ * 
+ * @Test void contextLoads() { }
+ * 
+ * }
+ */
